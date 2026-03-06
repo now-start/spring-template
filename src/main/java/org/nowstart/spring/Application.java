7a0398dc07a6b514cf4c2592d409564cb73dc2a2
moon <nowstart2691@gmail.com>
2026-03-07 00:23:18 +0900

    chore: Update Spring Boot to 4.0.3, Spring Cloud to 2025.1.1, refactor application.yaml, and streamline build dependencies

--- src/main/java/org/nowstart/spring/Application.java
+++ src/main/java/org/nowstart/spring/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,12 +14,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @EnableJpaAuditing
-@EnableFeignClients
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Application {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
