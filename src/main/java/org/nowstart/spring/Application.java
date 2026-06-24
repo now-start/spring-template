0ac11e9810748f06d67b26f6e69b6a0ba9d3ac4a
moon <nowstart2691@gmail.com>
2026-06-25 08:36:59 +0900

    chore: 스프링 템플릿 기본 설정 정리

--- src/main/java/org/nowstart/spring/Application.java
+++ src/main/java/org/nowstart/spring/Application.java
@@ -1,19 +1,17 @@
 package org.nowstart.spring;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
-@EnableRabbit
-@EnableCaching
 @EnableScheduling
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Application {
